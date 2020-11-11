75b38a157d9a35e3385dede35a9bfa9aa233386a
yingding <yingdingwang@yahoo.de>
2020-11-11 10:21:36 +0100

    updated get SparkSession with StringBuilder as a method param

--- playWithSparkMlExample/server/app/ml/WordCountExample.java
+++ playWithSparkMlExample/server/app/ml/WordCountExample.java
@@ -6,16 +6,17 @@ import org.apache.spark.sql.SparkSession;
 
 public class WordCountExample {
     public static final String TAG = WordCountExample.class.getSimpleName();
+    public static final StringBuilder TAG_BUILDER = new StringBuilder(TAG);
 
-    // Singleton ?
-    public static SparkSession getSparkSession() {
+    // TODO: convert this static method to a Singleton?
+    public static SparkSession getSparkSession(StringBuilder sb) {
         /* Spark Shell config example
          * Reference: https://spark.apache.org/docs/latest/
          * spark-shell --conf spark.executor.memory=1G --conf spark.executor.cores=1 --master spark://spark-master:7077
          */
         return SparkSession
                 .builder()
-                .appName(TAG) // name of the app
+                .appName(sb.toString()) // name of the app
                 .config("spark.executor.memory", "1G")
                 .config("spark.executor.cores", "1")
                 .master("spark://spark-master:7077")
@@ -24,10 +25,11 @@ public class WordCountExample {
 
     public void execute() {
         /* Open SparkSession and SparkContext */
-        SparkSession spark = getSparkSession();
+        SparkSession spark = getSparkSession(TAG_BUILDER);
         // Create a JavaSparkContext from a SparkSession's SparkContext object
         JavaSparkContext jsc = new JavaSparkContext(spark.sparkContext());
 
+        /* Code Block */
         // JavaRDD<String> textFile =
 
         /* Close SparkSession and SparkContext */
